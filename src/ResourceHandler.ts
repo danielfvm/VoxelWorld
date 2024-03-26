39e168df9fec5ce19fb6b7be7ea7c3bd9b985800
DeanCode <ds@xxxmail.eu>
2024-03-26 14:22:44 +0100

    Added relative paths

--- src/ResourceHandler.ts
+++ src/ResourceHandler.ts
@@ -8,12 +8,17 @@ export default class ResourceHandler {
   private static dataTable: {[url: string]: any} = {};
 
   public static async get(url: string, type: ResourceType = ResourceType.FILE): Promise<any> {
+    const config = require('config');
+    const path = config.path || "..";
+
+    console.log("Config path: " + path);
+
     if (!ResourceHandler.has(url)) {
       switch (type) {
         case ResourceType.FILE:
-          return ResourceHandler.dataTable[url] = await (await fetch(url)).text();
+          return ResourceHandler.dataTable[url] = await (await fetch(url.replace("..", path))).text();
         case ResourceType.AUDIO:
-          return ResourceHandler.dataTable[url] = new Audio(url);
+          return ResourceHandler.dataTable[url] = new Audio(url.replace("..", path));
         case ResourceType.TEXTURE:
           throw `Unimplemented type ${type}`;
       }
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
 //webpack.config.js
 const path = require('path');
 
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    hot: true
-  },
-  entry: {
-    main: "./src/app.ts"
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: "app-bundle.js" // <--- Will be compiled to this single file
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.tsx?$/,
-        loader: "ts-loader"
-      }
-    ]
-  },
-  devServer: {
-    static: {
-      directory: '.',
+module.exports = (env) => {
+  return {
+    mode: env.production ? "production" : "development",
+    externals: {
+      "config": JSON.stringify({
+        "path": env.path
+      }),
     },
-    port: 8081,
-    host: '0.0.0.0',
-    allowedHosts: 'all',
-  }
+    devServer: {
+      hot: true
+    },
+    entry: {
+      main: "./src/app.ts"
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: "app-bundle.js"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+      rules: [
+        { 
+          test: /\.tsx?$/,
+          loader: "ts-loader"
+        }
+      ]
+    },
+    devServer: {
+      static: {
+        directory: '.',
+      },
+      port: 8081,
+      host: '0.0.0.0',
+      allowedHosts: 'all',
+    }
+  };
 };
